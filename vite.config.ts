d294fe4b78bc5e2b98524d03288eceda86990499
Fabia Alves <fabia.k.alves15@gmail.com>
2026-07-27 11:52:33 -0300

    update base path in Vite config to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Substitua 'nome-do-seu-repositorio' pelo nome exato do repositório que você criar no GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/nome-do-seu-repositorio/', 
+  base: '/convite-civil/',
 })
